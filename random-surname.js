48a8ee9e9f2d2ba519d6342a40652b1cf4f7c819
li-zhaoyang <K_AEIx@163.com>
2021-07-11 19:10:15 +0800

    pasted pieces of code of generating surnames; fixed a bug in replacement script

---
+++ random-surname.js
@@ -0,0 +1,7 @@
+all = `李王张刘陈杨黄赵吴周徐孙马朱胡郭何高林罗郑梁谢宋唐许韩冯邓曹彭曾萧田董
+袁潘于蒋蔡余杜叶程苏魏吕丁任沈姚卢姜崔钟谭陆汪范金石廖贾夏韦傅方白邹孟熊秦邱江
+尹薛阎段雷侯`.replace("\n", '')
+console.log(all[Math.round(Math.random() * all.length)])
+
+all = ["欧阳", "令狐", "皇甫", "上官", "司徒", "诸葛", "司马", "宇文", "呼延", "端木"]
+console.log(all[Math.round(Math.random() * all.length)])
--- replace-words.py
+++ replace-words.py
@@ -79,88 +79,89 @@ def main():
     else:
       _search += '|({})'.format(escape(w, r'.^$*+?{}\[]|():<#=!'))
   paraendpat = re.compile(_search)
-  for fp in Path(re.sub(r'[^/]+$', '', config["select"])).glob(re.search(r'[^/]+$', config["select"]).group(0)):
-    restore = config.get("restore", None)
-    if config.get("min_update", False) and restore and len(restore) and Path(restore).exists():
-      if not re.search(r'\\|/$', restore):
-        restore += '/'
-      target = Path(restore + fp.name)
-      if target.exists() and fp.stat().st_mtime <= target.stat().st_mtime:
-        continue
-    content = fp.read_text(encoding = 'utf-8')
-    newlines = []    # tuple (index, replacespc)
-    paraends = []    # tuple (index, symbol)
-    pos = content.find('\n')
-    while pos >= 0:
-      nearbyparaend = paraendpat.search(content, pos = newlines[-1][0] if len(newlines) else 0)
-      if nearbyparaend and nearbyparaend.start() <= pos and nearbyparaend.end() > pos:
-        paraends.append((nearbyparaend.start(), nearbyparaend.group(0)))
-        content = content[ : nearbyparaend.start()] + ' ' + content[nearbyparaend.end() : ]
-      else:
-        _0 = ord(content[pos - 1]) < 256 and ord(content[pos + 1]) < 256
-        _1 = not content[pos - 1].isspace() and not content[pos + 1].isspace()
-        if _0 and _1:
-          newlines.append((pos, True))
-          content = content[ : pos] + ' ' + content[pos + 1 : ]
+  for glob in config["select"]:
+    for fp in Path(re.sub(r'[^/]+$', '', glob)).glob(re.search(r'[^/]+$', glob).group(0)):
+      restore = config.get("restore", None)
+      if config.get("min_update", False) and restore and len(restore) and Path(restore).exists():
+        if not re.search(r'\\|/$', restore):
+          restore += '/'
+        target = Path(restore + fp.name)
+        if target.exists() and fp.stat().st_mtime <= target.stat().st_mtime:
+          continue
+      content = fp.read_text(encoding = 'utf-8')
+      newlines = []    # tuple (index, replacespc)
+      paraends = []    # tuple (index, symbol)
+      pos = content.find('\n')
+      while pos >= 0:
+        nearbyparaend = paraendpat.search(content, pos = newlines[-1][0] if len(newlines) else 0)
+        if nearbyparaend and nearbyparaend.start() <= pos and nearbyparaend.end() > pos:
+          paraends.append((nearbyparaend.start(), nearbyparaend.group(0)))
+          content = content[ : nearbyparaend.start()] + ' ' + content[nearbyparaend.end() : ]
         else:
-          newlines.append((pos, False))
-          content = content[ : pos] + content[pos + 1 : ]
-      pos = content.find('\n', pos)
-    searchstart = 0
-    while True:
-      matched = searchpat.search(content, pos = searchstart)
-      if not matched:
-        break
-      found = False
-      for key in config["mappings"].keys():
-        for name in config["mappings"][key]:
-          if name == matched.group(0):
-            found = True
-            break
-        if found:
-          break
-      content = content[0 : matched.start()] + key + content[matched.end() : ]
-      searchstart = matched.start() + len(key)
-      if len(key) != len(name):
-        for i in range(0, len(newlines)):
-          if newlines[i][0] < matched.start():
-            continue
-          pos = newlines[i][0] + len(key) - len(name)
           _0 = ord(content[pos - 1]) < 256 and ord(content[pos + 1]) < 256
           _1 = not content[pos - 1].isspace() and not content[pos + 1].isspace()
           if _0 and _1:
-            newlines[i] = (pos, True)
+            newlines.append((pos, True))
+            content = content[ : pos] + ' ' + content[pos + 1 : ]
           else:
-            newlines[i] = (pos, False)
-        for i in range(0, len(paraends)):
-          if paraends[i][0] < matched.start():
-            continue
-          paraends[i] = (paraends[i][0] + len(key) - len(name), paraends[i][1])
-    i = 0
-    j = 0
-    mutations = []    # (idx, content, replace_or_insert)
-    while i < len(newlines) or j < len(paraends):
-      if i == len(newlines):
-        mutations.append((paraends[j][0], paraends[j][1], True))
-        j += 1
-      elif j == len(paraends) or newlines[i][0] < paraends[j][0]:
-        mutations.append((newlines[i][0], '\n', newlines[i][1]))
-        i += 1
+            newlines.append((pos, False))
+            content = content[ : pos] + content[pos + 1 : ]
+        pos = content.find('\n', pos)
+      searchstart = 0
+      while True:
+        matched = searchpat.search(content, pos = searchstart)
+        if not matched:
+          break
+        found = False
+        for key in config["mappings"].keys():
+          for name in config["mappings"][key]:
+            if name == matched.group(0):
+              found = True
+              break
+          if found:
+            break
+        content = content[0 : matched.start()] + key + content[matched.end() : ]
+        searchstart = matched.start() + len(key)
+        if len(key) != len(name):
+          for i in range(0, len(newlines)):
+            if newlines[i][0] < matched.start():
+              continue
+            pos = newlines[i][0] + len(key) - len(name)
+            _0 = ord(content[pos - 1]) < 256 and ord(content[pos + 1]) < 256
+            _1 = not content[pos - 1].isspace() and not content[pos + 1].isspace()
+            if _0 and _1:
+              newlines[i] = (pos, True)
+            else:
+              newlines[i] = (pos, False)
+          for i in range(0, len(paraends)):
+            if paraends[i][0] < matched.start():
+              continue
+            paraends[i] = (paraends[i][0] + len(key) - len(name), paraends[i][1])
+      i = 0
+      j = 0
+      mutations = []    # (idx, content, replace_or_insert)
+      while i < len(newlines) or j < len(paraends):
+        if i == len(newlines):
+          mutations.append((paraends[j][0], paraends[j][1], True))
+          j += 1
+        elif j == len(paraends) or newlines[i][0] < paraends[j][0]:
+          mutations.append((newlines[i][0], '\n', newlines[i][1]))
+          i += 1
+        else:
+          mutations.append((paraends[j][0], paraends[j][1], True))
+          j += 1
+      for mut in reversed(mutations):
+        if mut[2]:
+          content = content[ : mut[0]] + mut[1] + content[mut[0] + 1 : ]
+        else:
+          content = content[ : mut[0]] + mut[1] + content[mut[0] : ]
+      if config["restore"] and len(config["restore"]):
+        if not re.search(r'\\|/$', config['restore']):
+          config['restore'] += '/'
+        Path(config["restore"]).mkdir(parents = True, exist_ok = True)
+        Path(config["restore"] + fp.name).write_text(content, encoding = 'utf-8')
       else:
-        mutations.append((paraends[j][0], paraends[j][1], True))
-        j += 1
-    for mut in reversed(mutations):
-      if mut[2]:
-        content = content[ : mut[0]] + mut[1] + content[mut[0] + 1 : ]
-      else:
-        content = content[ : mut[0]] + mut[1] + content[mut[0] : ]
-    if config["restore"] and len(config["restore"]):
-      if not re.search(r'\\|/$', config['restore']):
-        config['restore'] += '/'
-      Path(config["restore"]).mkdir(parents = True, exist_ok = True)
-      Path(config["restore"] + fp.name).write_text(content, encoding = 'utf-8')
-    else:
-      fp.write_text(content, encoding = 'utf-8')
+        fp.write_text(content, encoding = 'utf-8')
     
 
 main() if __name__ == '__main__' else exit()
